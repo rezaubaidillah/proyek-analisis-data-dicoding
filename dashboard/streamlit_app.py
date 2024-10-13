01e91697fe2a20d769befa631a08f5d55ebb03e7
Reza Ubaidillah <161404112+rezaubaidillah@users.noreply.github.com>
2024-10-13 16:14:44 +0800

    Update streamlit_app.py

--- dashboard/streamlit_app.py
+++ dashboard/streamlit_app.py
@@ -227,9 +227,9 @@ def plot_casual_vs_registered(data, statistic_select):
 
 # Memuat data
 all_df = pd.read_csv(
-    "proyek-analisis-data-dicoding/dashboard/hour.csv")
+    "dashboard/hour.csv")
 day_df = pd.read_csv(
-    "proyek-analisis-data-dicoding/dashboard/day.csv")
+    "dashboard/day.csv")
 # Konversi kolom tanggal ke datetime
 all_df["dteday"] = pd.to_datetime(all_df["dteday"])
 day_df["dteday"] = pd.to_datetime(day_df["dteday"])
